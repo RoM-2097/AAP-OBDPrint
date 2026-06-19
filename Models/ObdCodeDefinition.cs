15ffc89a7e337ed54f8d3ee722b6ed711b2451ee
RoM-2097 <rmorrow@breezelineohio.net>
2026-06-18 21:30:36 -0400

    First commit of my project files.

---
+++ Models/ObdCodeDefinition.cs
@@ -0,0 +1,11 @@
+// using System;
+// using System.Collections.Generic;
+
+namespace AAP_OBDPrint.Models
+{
+    public class OBDResult
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
